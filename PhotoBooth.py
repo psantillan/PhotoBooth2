4edcaed536c14c6909490a42702cf99b81760a36
psantillan <paul.r.santillan@gmail.com>
2022-12-06 19:21:31 -0800

    refactor to PyGame from Qt

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -1,49 +1,42 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QApplication, QWidget
-from picamera2.previews.qt import QGlPicamera2
+import pygame
 from picamera2 import Picamera2
+
+class Camera:
+    def __init__(self, *args, **kwargs):
+        # initialize picamera2
+        self.camera = Picamera2()
+        # create sensor configs
+        self.mode = {
+
+            'preview': self.camera.create_preview_configuration(),
+            'still': self.camera.create_still_configuration()
+        }
+        # apply the configuration
+        self.camera.configure(self.mode['preview'])
+        # start camera
+        self.camera.start()
+
+    def __getattr__(self, attr):
+        return getattr(self.camera, attr)
 
 
 class PhotoBooth:
-    def __init__(self, window):
-        window.resize(800, 600)
-        self.picam2 = Picamera2()
-        self.config = {
-            'preview': self.picam2.create_preview_configuration(),
-            'still': self.picam2.create_still_configuration()
-        }
-        self.picam2.configure(self.config['preview'])
-        self.qpicamera2 = QGlPicamera2(self.picam2, width=1280, keep_ar=True)
-        self.qpicamera2.done_signal.connect(self.capture_done)
-        self.picam2.start()
+    def __init__(self, camera, *args, **kwargs):
+        pygame.init()
+        self.camera = camera
+        self.size = (640, 480) if 'window_size' not in kwargs else kwargs['window_size']
+        self.window = pygame.display.set_mode(self.size)
 
-
-        self.layout = QVBoxLayout()
-        self.button = QPushButton("Click to capture")
-        self.button.clicked.connect(self.capture)
-        self.setup_ui()
-        window.setLayout(self.layout)
-
-    def setup_ui(self):
-        self.button.setGeometry(200, 150, 100, 40)
-        self.layout.addWidget(self.qpicamera2)
-        self.layout.addWidget(self.button)
-
-    def capture_done(self):
-        x = self.picam2.wait()
-        print(x)
-        self.button.setEnabled(True)
-        self.button.setText("Click to capture")
-
-    def capture(self):
-        self.button.setEnabled(False)
-        self.button.setText('Capturing....')
-        self.picam2.switch_mode_and_capture_file(self.config['still'], 'test.jpg', signal_function=self.qpicamera2.signal_done)
+    def run(self):
+        running = True
+        while running:
+            self.window.fill((255, 255, 255))
+            cam_data = self.camera.capture_buffer()
+            cam_surface = pygame.image.frombuffer(cam_data, (640,480), 'RGBA')
+            self.window.blit(cam_surface, (0, 0))
+            pygame.display.update()
 
 
 if __name__ == "__main__":
-    app = QApplication([])
-    MainWindow = QWidget()
-    pb = PhotoBooth(MainWindow)
-    MainWindow.show()
-    app.exec()
+    pb = PhotoBooth(Camera())
+    pb.run()
