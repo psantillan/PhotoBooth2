60c4f8fdc5cdc5349d2e5ff811b68bd2595d990f
psantillan <paul.r.santillan@gmail.com>
2022-12-08 13:47:18 -0800

    Capture debugging

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -13,7 +13,7 @@ class Camera:
         # create sensor configs
         self.mode = {
 
-            'preview': self.camera.create_video_configuration({'size': self.size}, raw=self.camera.sensor_modes[2]),
+            'preview': self.camera.create_video_configuration({'size': self.size}),
             #'preview': self.camera.create_video_configuration( lores={"size": self.size}, encode="lores"),
             'still': self.camera.create_still_configuration()
         }
