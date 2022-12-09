1eef32fee886b147597a0e4f2c61483c0a494d8d
psantillan <paul.r.santillan@gmail.com>
2022-12-08 19:06:58 -0800

    Camera debug

--- main.py
+++ main.py
@@ -3,7 +3,4 @@ from Camera import Camera
 import pygame
 
 if __name__ == '__main__':
-    with Camera as camera:
-        with PhotoBooth as pb:
-            while True:
-                pygame.display.update()
+    print(Camera)
