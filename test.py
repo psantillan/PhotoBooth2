b63f4d72105848b19ad0c854a68f71465fc07a72
psantillan <paul.r.santillan@gmail.com>
2022-12-19 21:24:02 -0800

    Buffer testing

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ class PyGameCamera:
         pygame.camera.init()
         self.window = pygame.display.set_mode((700, 500), pygame.NOFRAME)
         self.camera = pygame.camera.Camera('/dev/media1')
+        print(self.camera.list_cameras())
         #self.camera.stop()
-        self.camera.start()
+        #self.camera.start()
 
     def run(self):
         while True:
